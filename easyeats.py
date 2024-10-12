66c659f5135c13f199d8b76fb3c3984d22a3677e
Carsonb18 <carson.brown2266@gmail.com>
2024-10-12 23:59:15 +0000

    Added ai for recipie handling

--- easyeats.py
+++ easyeats.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
 
-st.title("🎈 Easy Eats ")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+#load environment variables
+load_dotenv('codespace.env')
+
+# Retrieve the API key
+api_keyy = os.getenv('GOOGLE_API_KEY')
+if api_keyy is None:
+    st.error("API key not found. Please check your .env file.")
+else:
+    genai.configure(api_key=api_keyy)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+user_input = st.text_input("Enter the ingredients you would like to use: ")
+
+if user_input:
+    response = model.generate_content("Give me a recipie I can make with the following ingredients: " + user_input)
+    # Display the response in the Streamlit app
+    st.write(response.text)
+
